985b35a10152f694221906eefe352cd99b0efe94
Jonathan <jontyphilipt3@gmail.com>
2024-05-31 19:57:59 +0530

    environment set up
    shadcn-ui color problem needs to fix.

---
+++ lms/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+    <p className="text-3xl font-medium text-sky-700">
+      hello world
+    </p>
+    
+    <Button variant="ghost">
+      click me
+    </Button>
+    </div>
+  )
+}
