ff996ff8efb8aa70c3150516b25efd236f78aadd
Jonathan <jontyphilipt3@gmail.com>
2024-06-05 18:55:26 +0530

    course creation ui
    implemented toaster-hot react and form filling for course creation

--- lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
     return ( 
-        <div>courses page</div>
+        <div className="p-6 ">
+            <Link href="/teacher/create">
+            <Button className="bg-sky-300">
+                 New Course
+            </Button>
+            </Link>
+        </div>
      );
 }
  
